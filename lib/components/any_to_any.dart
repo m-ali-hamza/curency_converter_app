aeed60f4106158c126604600884545c76126e374
m-ali-hamza <alichaishai@gmail.com>
2023-12-27 14:41:51 +0500

    generated flutter icon

--- lib/components/any_to_any.dart
+++ lib/components/any_to_any.dart
@@ -11,6 +11,7 @@ class AnyToAny extends StatefulWidget {
 
 class _AnyToAnyState extends State<AnyToAny> {
   TextEditingController controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   String dropdownValue1 = "USD";
   String dropdownValue2 = "PKR";
@@ -25,161 +26,174 @@ class _AnyToAnyState extends State<AnyToAny> {
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: height / 60, vertical: height / 60),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Text("USD to Any Currency",
-                style: TextStyle(
-                    color: Colors.grey.shade300,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18)),
-          ),
-          SizedBox(
-            height: height / 50,
-          ),
-          TextFormField(
-            controller: controller,
-            keyboardType: TextInputType.number,
-            style: TextStyle(
-                color: Colors.grey.shade300, fontWeight: FontWeight.bold),
-            decoration: InputDecoration(
-              hintText: "USD",
-              prefixIcon: Icon(
-                Icons.attach_money,
-                color: Colors.grey.shade300,
-              ),
-              fillColor: Colors.grey.shade600,
-              filled: true,
-              hintStyle: TextStyle(
-                  color: Colors.grey.shade300, fontWeight: FontWeight.bold),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(10)),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text("USD to Any Currency",
+                  style: TextStyle(
+                      color: Colors.grey.shade300,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18)),
             ),
-          ),
-          SizedBox(
-            height: height / 30,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: DropdownButton(
-                    borderRadius: BorderRadius.circular(10),
-                    dropdownColor: Colors.grey.shade900,
-                    isExpanded: true,
-                    icon: Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.grey.shade300,
-                    ),
-                    style: TextStyle(
+            SizedBox(
+              height: height / 50,
+            ),
+            TextFormField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                  color: Colors.grey.shade300, fontWeight: FontWeight.bold),
+              decoration: InputDecoration(
+                hintText: dropdownValue1,
+                prefixIcon: Icon(
+                  Icons.attach_money,
+                  color: Colors.grey.shade300,
+                ),
+                fillColor: Colors.grey.shade600,
+                filled: true,
+                hintStyle: TextStyle(
+                    color: Colors.grey.shade300, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Amount';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: height / 30,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: DropdownButton(
+                      borderRadius: BorderRadius.circular(10),
+                      dropdownColor: Colors.grey.shade900,
+                      isExpanded: true,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
                         color: Colors.grey.shade300,
-                        fontWeight: FontWeight.bold),
-                    iconSize: width / 13,
-                    value: dropdownValue1,
-                    items: widget.rates.keys
-                        .toList()
-                        .map<DropdownMenuItem<String>>((value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (value) {
-                      dropdownValue1 = value!;
-                      setState(() {});
-                    },
+                      ),
+                      style: TextStyle(
+                          color: Colors.grey.shade300,
+                          fontWeight: FontWeight.bold),
+                      iconSize: width / 13,
+                      value: dropdownValue1,
+                      items: widget.rates.keys
+                          .toList()
+                          .map<DropdownMenuItem<String>>((value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      onChanged: (value) {
+                        dropdownValue1 = value!;
+                        setState(() {});
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Icon(
-                Icons.currency_exchange,
-                color: Colors.grey.shade300,
-                size: 30,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: DropdownButton(
-                    borderRadius: BorderRadius.circular(10),
-                    dropdownColor: Colors.grey.shade900,
-                    isExpanded: true,
-                    icon: Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.grey.shade300,
-                    ),
-                    style: TextStyle(
+                Icon(
+                  Icons.currency_exchange,
+                  color: Colors.grey.shade300,
+                  size: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: DropdownButton(
+                      borderRadius: BorderRadius.circular(10),
+                      dropdownColor: Colors.grey.shade900,
+                      isExpanded: true,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
                         color: Colors.grey.shade300,
-                        fontWeight: FontWeight.bold),
-                    iconSize: width / 13,
-                    value: dropdownValue2,
-                    items: widget.rates.keys
-                        .toList()
-                        .map<DropdownMenuItem<String>>((value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (value) {
-                      dropdownValue2 = value!;
-                      setState(() {});
-                    },
+                      ),
+                      style: TextStyle(
+                          color: Colors.grey.shade300,
+                          fontWeight: FontWeight.bold),
+                      iconSize: width / 13,
+                      value: dropdownValue2,
+                      items: widget.rates.keys
+                          .toList()
+                          .map<DropdownMenuItem<String>>((value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      onChanged: (value) {
+                        dropdownValue2 = value!;
+                        setState(() {});
+                      },
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: FilledButton(
-                    onPressed: () {
-                      amount1 = anyToAnyCurrency(
-                          double.parse(controller.text),
-                          widget.rates[dropdownValue1]!,
-                          widget.rates[dropdownValue2]!);
-                      setState(() {});
-                    },
-                    style: FilledButton.styleFrom(
-                      backgroundColor: Colors.grey.shade600,
-                      foregroundColor: Colors.grey.shade300,
-                    ),
-                    child: const Text("Convert")),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: height / 100,
-          ),
-          Text(
-            "1 $dropdownValue1 = ${(widget.rates[dropdownValue2]) / (widget.rates[dropdownValue1])} $dropdownValue2",
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 12),
-          ),
-          SizedBox(
-            height: height / 100,
-          ),
-          Text(
-            "Converted Amount",
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 18),
-          ),
-          Text(
-            "${controller.text} $dropdownValue1 * ${(widget.rates[dropdownValue2]) / (widget.rates[dropdownValue1])} $dropdownValue2 = $amount1 $dropdownValue2",
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 16),
-          )
-        ],
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: FilledButton(
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        } else {
+                          amount1 = anyToAnyCurrency(
+                              double.parse(controller.text),
+                              widget.rates[dropdownValue1]!,
+                              widget.rates[dropdownValue2]!);
+                          setState(() {});
+                        }
+                      },
+                      style: FilledButton.styleFrom(
+                        backgroundColor: Colors.grey.shade600,
+                        foregroundColor: Colors.grey.shade300,
+                      ),
+                      child: const Text("Convert")),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height / 100,
+            ),
+            Text(
+              "1 $dropdownValue1 = ${double.parse(((widget.rates[dropdownValue2]) / (widget.rates[dropdownValue1])).toStringAsFixed(2))} $dropdownValue2",
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12),
+            ),
+            SizedBox(
+              height: height / 100,
+            ),
+            Text(
+              "Converted Amount",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+            Text(
+              "${controller.text} $dropdownValue1 * ${double.parse(((widget.rates[dropdownValue2]) / (widget.rates[dropdownValue1])).toStringAsFixed(2))} $dropdownValue2 = $amount1 $dropdownValue2",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/components/usd_to_any.dart
+++ lib/components/usd_to_any.dart
@@ -11,7 +11,7 @@ class UsdToAny extends StatefulWidget {
 
 class _UsdToAnyState extends State<UsdToAny> {
   TextEditingController usdController = TextEditingController();
-
+  final _formKey = GlobalKey<FormState>();
   String dropdownValue = "PKR";
 
   double usdAmount = 0.0;
@@ -19,124 +19,138 @@ class _UsdToAnyState extends State<UsdToAny> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    usdController.text = '0.0';
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: height / 60, vertical: height / 60),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Text("USD to Any Currency",
-                style: TextStyle(
-                    color: Colors.grey.shade300,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18)),
-          ),
-          SizedBox(
-            height: height / 50,
-          ),
-          TextFormField(
-            controller: usdController,
-            keyboardType: TextInputType.number,
-            style: TextStyle(
-                color: Colors.grey.shade300, fontWeight: FontWeight.bold),
-            decoration: InputDecoration(
-              hintText: "USD",
-              prefixIcon: Icon(
-                Icons.attach_money,
-                color: Colors.grey.shade300,
-              ),
-              fillColor: Colors.grey.shade600,
-              filled: true,
-              hintStyle: TextStyle(
-                  color: Colors.grey.shade300, fontWeight: FontWeight.bold),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          SizedBox(
-            height: height / 30,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: DropdownButton(
-                    borderRadius: BorderRadius.circular(10),
-                    dropdownColor: Colors.grey.shade900,
-                    isExpanded: true,
-                    icon: Icon(
-                      Icons.arrow_drop_down,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text("USD to Any Currency",
+                  style: TextStyle(
                       color: Colors.grey.shade300,
-                    ),
-                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18)),
+            ),
+            SizedBox(
+              height: height / 50,
+            ),
+            TextFormField(
+              controller: usdController,
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                  color: Colors.grey.shade300, fontWeight: FontWeight.bold),
+              decoration: InputDecoration(
+                hintText: "USD",
+                prefixIcon: Icon(
+                  Icons.attach_money,
+                  color: Colors.grey.shade300,
+                ),
+                fillColor: Colors.grey.shade600,
+                filled: true,
+                hintStyle: TextStyle(
+                    color: Colors.grey.shade300, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Amount';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: height / 30,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: DropdownButton(
+                      borderRadius: BorderRadius.circular(10),
+                      dropdownColor: Colors.grey.shade900,
+                      isExpanded: true,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
                         color: Colors.grey.shade300,
-                        fontWeight: FontWeight.bold),
-                    iconSize: width / 13,
-                    value: dropdownValue,
-                    items: widget.rates.keys
-                        .toList()
-                        .map<DropdownMenuItem<String>>((value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (value) {
-                      dropdownValue = value!;
-                      setState(() {});
-                    },
+                      ),
+                      style: TextStyle(
+                          color: Colors.grey.shade300,
+                          fontWeight: FontWeight.bold),
+                      iconSize: width / 13,
+                      value: dropdownValue,
+                      items: widget.rates.keys
+                          .toList()
+                          .map<DropdownMenuItem<String>>((value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      onChanged: (value) {
+                        dropdownValue = value!;
+                        setState(() {});
+                      },
+                    ),
                   ),
                 ),
-              ),
-              FilledButton(
-                  onPressed: () {
-                    usdAmount = usdToAnyCurrency(
-                        double.parse(usdController.text),
-                        widget.rates[dropdownValue]!);
-                    setState(() {});
-                  },
-                  style: FilledButton.styleFrom(
-                    backgroundColor: Colors.grey.shade600,
-                    foregroundColor: Colors.grey.shade300,
-                  ),
-                  child: const Text("Convert"))
-            ],
-          ),
-          SizedBox(
-            height: height / 100,
-          ),
-          Text(
-            "1 USD = ${widget.rates[dropdownValue]} $dropdownValue",
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 12),
-          ),
-          SizedBox(
-            height: height / 100,
-          ),
-          Text(
-            "Converted Amount",
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 18),
-          ),
-          Text(
-            "${usdController.text} * ${widget.rates[dropdownValue]} = $usdAmount $dropdownValue",
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                color: Colors.grey.shade300,
-                fontWeight: FontWeight.bold,
-                fontSize: 16),
-          )
-        ],
+                FilledButton(
+                    onPressed: () {
+                      //form validation
+                      if (!_formKey.currentState!.validate()) {
+                        return;
+                      } else {
+                        usdAmount = usdToAnyCurrency(
+                            double.parse(usdController.text),
+                            widget.rates[dropdownValue]!);
+                        setState(() {});
+                      }
+                    },
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.grey.shade600,
+                      foregroundColor: Colors.grey.shade300,
+                    ),
+                    child: const Text("Convert"))
+              ],
+            ),
+            SizedBox(
+              height: height / 100,
+            ),
+            Text(
+              "1 USD = ${double.parse(widget.rates[dropdownValue].toStringAsFixed(2))} $dropdownValue",
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12),
+            ),
+            SizedBox(
+              height: height / 100,
+            ),
+            Text(
+              "Converted Amount",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+            Text(
+              "${usdController.text} * ${double.parse(widget.rates[dropdownValue].toStringAsFixed(2))} = $usdAmount $dropdownValue",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey.shade300,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            )
+          ],
+        ),
       ),
     );
   }
